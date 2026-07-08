61b0e4e6e752ad0ae1a067bfc28a638d4a734c1d
francoiswallet1-Fr <francois.wallet1@gmail.com>
2026-07-08 08:37:26 +0200

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,30 @@
+const CACHE_NAME = 'grimoire-ak-v1';
+const ASSETS = [
+  '/grimoire-AK/',
+  '/grimoire-AK/index.html',
+  '/grimoire-AK/icon-192x192.png',
+  '/grimoire-AK/icon-512x512.png',
+  '/grimoire-AK/favicon.ico'
+];
+
+self.addEventListener('install', e => {
+  e.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(ASSETS))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', e => {
+  e.waitUntil(
+    caches.keys().then(keys =>
+      Promise.all(keys.filter(k => k !== CACHE_NAME).map(k => caches.delete(k)))
+    )
+  );
+  self.clients.claim();
+});
+
+self.addEventListener('fetch', e => {
+  e.respondWith(
+    caches.match(e.request).then(cached => cached || fetch(e.request))
+  );
+});
